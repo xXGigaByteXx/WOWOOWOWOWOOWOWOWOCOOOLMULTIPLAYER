a8a3f2928e6a32fd7181e8c276120feffc5f5fbe
Shuhul Raina <developer@MacBook-Pro.local>
2021-03-26 15:56:50 -0700

    MULTIPLAYER

--- sketch.js
+++ sketch.js
@@ -1,29 +1,44 @@
 var ball;
+var database;
+var pos;
 
-function setup(){
+function setup() {
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPos = database.ref('ball/position');
+    ballPos.on("value", readPosition, showError)
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(keyDown("a")){
+        writePosition(-5,0);
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+    else if(keyDown("d")){
+        writePosition(5,0);
     }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+    else if(keyDown("w")){
+        writePosition(0,-5);
     }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+    else if(keyDown("s")){
+        writePosition(0,+5);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': pos.x + x,
+        'y': pos.y + y,
+    });
+}
+function readPosition(data) {
+    pos = data.val();
+    ball.x = pos.x;
+    ball.y = pos.y;
+}
+function showError() {
+    console.log("error database writing... thing");
 }
